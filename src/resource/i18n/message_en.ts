fcf8b6bedaf62b0c37d4f8dc17b213743d00657e
kittybbit <kittybbit@gmail.com>
2024-01-08 13:15:58 +0900

    chenge menu title

--- src/resource/i18n/message_en.ts
+++ src/resource/i18n/message_en.ts
@@ -1,8 +1,8 @@
 export const message_en = {
     'search.helperText': 'Search by enter or focus out.',
     'menu.menuItem1': 'Select display columns.',
-    'menu.menuItem2.static': 'Switch to show all rows.',
-    'menu.menuItem2.virtual': 'Switch to partial row view on scroll.',
+    'menu.menuItem2.static': 'Toggle window scroll.',
+    'menu.menuItem2.virtual': 'Toggle table scroll.',
     'columnSelectSidebar.invisibleAll': 'All columns to invisible.',
     'columnSelectSidebar.visibleAll': 'All columns to visible.',
 }
