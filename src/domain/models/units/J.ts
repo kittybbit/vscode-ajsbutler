204c300e9f4628b583c858b7096b5cb23ec3f72c
kittybbit <kittybbit@gmail.com>
2024-11-16 11:51:31 +0900

    Fix and refactored J
    - Fixed the issue with the sd2 property.
    - Refactored the topN property for improved clarity.

--- src/domain/models/units/J.ts
+++ src/domain/models/units/J.ts
@@ -60,25 +60,25 @@ export class J extends UnitEntity {
     // [td1="transfer-destination-file-name-1";]
     get td1() { return ParamFactory.td1(this); }
     // [top1={sav|del};]
-    get top1() { return ParamFactory.top1(this, this.ts1 && this.td1 ? 'sav' : this.ts1 && !this.td1 ? 'del' : ''); }
+    get top1() { return ParamFactory.top1(this, this.#top(1)); }
     // [ts2="transfer-source-file-name-2";]
-    get ts2() { return ParamFactory.td2(this); }
+    get ts2() { return ParamFactory.ts2(this); }
     // [td2="transfer-destination-file-name-2";]
     get td2() { return ParamFactory.td2(this); }
     // [top2={sav|del};]
-    get top2() { return ParamFactory.top2(this, this.ts2 && this.td2 ? 'sav' : this.ts2 && !this.td2 ? 'del' : ''); }
+    get top2() { return ParamFactory.top2(this, this.#top(2)); }
     // [ts3="transfer-source-file-name-3";]
     get ts3() { return ParamFactory.ts3(this) }
     // [td3="transfer-destination-file-name-3";]
     get td3() { return ParamFactory.td3(this) }
     // [top3={sav|del};]
-    get top3() { return ParamFactory.top3(this, this.ts3 && this.td3 ? 'sav' : this.ts3 && !this.td3 ? 'del' : ''); }
+    get top3() { return ParamFactory.top3(this, this.#top(3)); }
     // [ts4="transfer-source-file-name-4";]
     get ts4() { return ParamFactory.ts4(this); }
     // [td4="transfer-destination-file-name-4";]
     get td4() { return ParamFactory.td4(this); }
     // [top4={sav|del};]
-    get top4() { return ParamFactory.top4(this, this.ts4 && this.td4 ? 'sav' : this.ts4 && !this.td4 ? 'del' : ''); }
+    get top4() { return ParamFactory.top4(this, this.#top(4)); }
     // [ha={y|n};]
     get ha() { return ParamFactory.ha(this, 'n'); }
     // [eu={ent|def};]
@@ -118,6 +118,14 @@ export class J extends UnitEntity {
     /** Whether this jobnet have a unit whose end is being waited for. */
     get hasWaitedFor() {
         return this.eun && this.eun.length > 0;
+    }
+
+    #top = (i: 1 | 2 | 3 | 4) => {
+        return this[`ts${i}`] && this[`td${i}`]
+            ? 'sav'
+            : this[`ts${i}`] && !this[`td${i}`]
+                ? 'del'
+                : '';
     }
 }
 export class Rj extends J { }
