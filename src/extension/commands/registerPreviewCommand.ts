5006b8ca61d97c8ae6a1aa0b796bcffb7f6c4661
kittybbit <kittybbit@gmail.com>
2026-04-10 22:33:52 +0900

    refactor: decouple preview registration

--- src/extension/commands/registerPreviewCommand.ts
+++ src/extension/commands/registerPreviewCommand.ts
@@ -1,17 +1,9 @@
 import * as vscode from "vscode";
-import { MyExtension } from "../MyExtension";
-import {
-  openPreviewCommand,
-  type PreviewPanelFactory,
-} from "./openPreviewCommand";
 
 export const registerPreviewCommand = (
   viewType: string,
-  panelFactory: PreviewPanelFactory,
-  myExtension: MyExtension,
+  execute: () => void,
 ): vscode.Disposable => {
   console.log(`invoke registerPreview. (${viewType})`);
-  return vscode.commands.registerCommand(`open.${viewType}`, () => {
-    openPreviewCommand(viewType, panelFactory, myExtension);
-  });
+  return vscode.commands.registerCommand(`open.${viewType}`, execute);
 };
